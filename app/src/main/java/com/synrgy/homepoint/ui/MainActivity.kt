b221394e84e5aade2557fceb2a000f4a512f1928
Mufti Ramdhani <muftiramdhani25@gmail.com>
2022-05-31 18:52:16 +0300

    init bottom navigation in homepage

--- app/src/main/java/com/synrgy/homepoint/ui/MainActivity.kt
+++ app/src/main/java/com/synrgy/homepoint/ui/MainActivity.kt
@@ -2,11 +2,46 @@ package com.synrgy.homepoint.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import com.synrgy.homepoint.R
+import com.synrgy.homepoint.databinding.ActivityMainBinding
+import com.synrgy.homepoint.ui.home.HomeFragment
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        initBottomNavigation()
+    }
+
+    private fun initBottomNavigation() {
+        binding.bnvMain.setOnItemSelectedListener { item ->
+            when(item.itemId){
+                R.id.menu_main_home -> {
+                    replaceFragment(HomeFragment())
+                    true
+                }
+                else -> false
+            }
+        }
+    }
+
+    // replace fragment
+    private fun replaceFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fl_main, fragment)
+            .commit()
+    }
+
+    // stop fragment
+    private fun stopFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+            .remove(fragment)
+            .commit()
     }
 }
