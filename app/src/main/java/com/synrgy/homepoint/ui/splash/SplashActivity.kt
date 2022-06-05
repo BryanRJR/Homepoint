ebf2ca18d6c436e0eb9e1bc7e8a3a37e9833f351
fathirmansyah <fathirmansyah@gmail.com>
2022-06-05 13:01:00 +0800

    Add SplashScreen

--- app/src/main/java/com/synrgy/homepoint/ui/splash/SplashActivity.kt
+++ app/src/main/java/com/synrgy/homepoint/ui/splash/SplashActivity.kt
@@ -1,12 +1,28 @@
 package com.synrgy.homepoint.ui.splash
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import com.synrgy.homepoint.R
+import com.synrgy.homepoint.ui.MainActivity
 
+@SuppressLint("CustomSplashScreen")
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+        Handler(Looper.getMainLooper())
+            .postDelayed({
+                startActivity(Intent(this, MainActivity::class.java))
+                finish()
+            }, SPLASH_TIME_OUT)
+    }
+
+    companion object {
+        private const val SPLASH_TIME_OUT = 3000L
     }
 }
