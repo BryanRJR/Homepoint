a29aa9c084769e43adde98dba2c67665a8d9e368
BryanRJR <bryanrantung98@gmail.com>
2022-06-06 10:13:11 +0700

    init feature login and register ui

--- app/src/main/java/com/synrgy/homepoint/ui/register/RegisterActivity.kt
+++ app/src/main/java/com/synrgy/homepoint/ui/register/RegisterActivity.kt
@@ -1,5 +1,6 @@
 package com.synrgy.homepoint.ui.register
 
+import android.net.LinkAddress
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.synrgy.homepoint.R
@@ -8,5 +9,7 @@ class RegisterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+
     }
+
 }
