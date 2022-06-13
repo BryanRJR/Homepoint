5e63fc8d8798344392fcd0c10c4b68e66f0db4d9
fathirmansyah <fathirmansyah@gmail.com>
2022-06-13 18:44:25 +0800

    Add search activity

---
+++ app/src/main/java/com/synrgy/homepoint/SearchActivity.kt
@@ -0,0 +1,11 @@
+package com.synrgy.homepoint
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SearchActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_search)
+    }
+}
