cf59564cb6b88d367ab178428262770a0a6d4d10
BryanRJR <bryanrantung98@gmail.com>
2022-06-19 18:28:40 +0700

    update loginRegister

--- app/src/main/java/com/synrgy/homepoint/ui/payment/PaymentActivity.kt
+++ app/src/main/java/com/synrgy/homepoint/ui/payment/PaymentActivity.kt
@@ -8,36 +8,11 @@ import com.synrgy.homepoint.ui.home.DataSource
 
 class PaymentActivity : AppCompatActivity() {
     private lateinit var binding: ActivityPaymentBinding
-    private var listDelivery: ArrayList<Delivery> = arrayListOf()
-    private var listOrder : ArrayList<Order> = arrayListOf()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityPaymentBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        setRecyclerViewDelivery()
-
-        setRecyclerViewOrder()
-    }
-    private fun setRecyclerViewOrder(){
-        listOrder.addAll(DataSource.listDataOrder)
-        binding.rvOrderList.apply {
-            setHasFixedSize(true)
-            isNestedScrollingEnabled = false
-            layoutManager = LinearLayoutManager(context, LinearLayoutManager.VERTICAL, false)
-            adapter = OrderAdapter(listOrder)
-        }
-
-    }
-
-    private fun setRecyclerViewDelivery() {
-        listDelivery.addAll(DataSource.listDataDelivery)
-        binding.rvDelivery.apply {
-            setHasFixedSize(true)
-            isNestedScrollingEnabled = false
-            layoutManager = LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false)
-            adapter = PaymentAdapter(listDelivery)
-        }
     }
 }
